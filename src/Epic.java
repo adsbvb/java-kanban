3429d4fee70e0b6d8c4a77453a382c6dcb2aae44
Daggett <adsbvb@yandex.ru>
2025-02-13 21:53:34 +0500

    Спринт 4. Трекер задач

---
+++ src/Epic.java
@@ -0,0 +1,16 @@
+public class Epic extends Task{
+
+    public Epic(String name, String description, Status status) {
+        super(name, description, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ src/Subtask.java
@@ -0,0 +1,16 @@
+public class Subtask extends Task{
+
+    public Subtask(String name, String description, Status status) {
+        super(name, description, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                '}';
+    }
+}
