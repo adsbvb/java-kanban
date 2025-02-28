718113a99d286cde8d4ad4e95d74aec22e85ddef
Daggett <adsbvb@yandex.ru>
2025-03-01 03:20:29 +0500

    InMemoryHistoryManagerTest

---
+++ test/tracker/controllers/InMemoryHistoryManagerTest.java
@@ -0,0 +1,30 @@
+package tracker.controllers;
+
+import org.junit.jupiter.api.*;
+import tracker.model.Status;
+import tracker.model.Task;
+
+class InMemoryHistoryManagerTest {
+
+    TaskManager taskManager = Managers.getDefault();
+
+    @Test
+    void addHistory() {
+        Managers.getDefaultHistory().clear();
+        int id1 = taskManager.createTask(new Task("Name", "Description", Status.NEW));
+        taskManager.getTask(id1);
+        int id2 = taskManager.createTask(new Task("NewName", "NewDescription", Status.NEW));
+        taskManager.getTask(id2);
+        Assertions.assertEquals(taskManager.getTask(id1), Managers.getDefaultHistory().getFirst(), "Предыдущая версия задачи не сохранилась!");
+    }
+
+    @Test
+    void checkLimitOfHistoryListInMemoryHistoryManager() {
+        Task testTask = new Task("Name", "Description", Status.NEW);
+        int id = taskManager.createTask(testTask);
+        for (int i = 0; i <= 12; i++) {
+            taskManager.getTask(id);
+        }
+        Assertions.assertEquals(10, Managers.getDefaultHistory().size(), "Лимит списка истории просмотра задач превышен!");
+    }
+}
