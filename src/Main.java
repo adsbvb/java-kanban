490a759efeb9b68a4842b8dcf1b4665c093f05ea
Daggett <adsbvb@yandex.ru>
2025-03-31 22:31:03 +0500

    Local variable name must match pattern '^[a-z]([a-zA-Z0-9]*)?$'.

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,11 @@ public class Main {
         int task2 = taskManager.createTask(new Task("Уборка.", Status.NEW, "Протереть пыль."));
 
         int epic1 = taskManager.createEpic(new Epic("Путевка.", Status.NEW, "Выбрать маршрут."));
-        int subtask1_1 = taskManager.addSubtask(new Subtask("Билеты.", Status.NEW, "Купить.", 3));
-        int subtask1_2 = taskManager.addSubtask(new Subtask("Личные вещи.", Status.NEW, "Собрать чемодан.", 3));
+        int subtask11 = taskManager.addSubtask(new Subtask("Билеты.", Status.NEW, "Купить.", 3));
+        int subtask12 = taskManager.addSubtask(new Subtask("Личные вещи.", Status.NEW, "Собрать чемодан.", 3));
 
         int epic2 = taskManager.createEpic(new Epic("Изучить новую специальность.", Status.NEW, "Освоить язык программирования Java."));
-        int subtask2_1 = taskManager.addSubtask(new Subtask("Учеба.", Status.NEW, "Пройти курс на Яндекс-Практикум.", 6));
+        int subtask21 = taskManager.addSubtask(new Subtask("Учеба.", Status.NEW, "Пройти курс на Яндекс-Практикум.", 6));
 
         TaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(file);
         System.out.println(fileBackedTaskManager.getTasks());
