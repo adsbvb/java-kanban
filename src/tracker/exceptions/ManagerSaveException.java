a589fc971615194cc8f4f1fa122745de7ab50dda
Daggett <adsbvb@yandex.ru>
2025-04-01 22:25:19 +0500

    Добавить ManagerSaveException в отдельный пакет exceptions.

--- src/tracker/exceptions/ManagerSaveException.java
+++ src/tracker/exceptions/ManagerSaveException.java
@@ -1,4 +1,4 @@
-package tracker.controllers;
+package tracker.exceptions;
 
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message, Throwable cause) {
