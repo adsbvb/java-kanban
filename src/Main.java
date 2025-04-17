2cc0134bfb6e0f048bdfec7823e394c0cdd12355
Daggett <adsbvb@yandex.ru>
2025-04-17 17:53:33 +0500

    sprint_8-solution-time-and-duration

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import tracker.controllers.FileBackedTaskManager;
 import tracker.controllers.Managers;
 import tracker.controllers.TaskManager;
 import tracker.model.*;
@@ -18,26 +17,25 @@ public class Main {
 
         TaskManager taskManager = Managers.getDefault(file);
 
-        int task1 = taskManager.createTask(new Task("Покупка.", Status.NEW, "Купить продукты."));
-        int task2 = taskManager.createTask(new Task("Уборка.", Status.NEW, "Протереть пыль."));
+        taskManager.createTask(new Task("Task1", "Description1", 2025, 5, 12, 2, 30, 120));
+        taskManager.createTask(new Task("Task2", "Description2", 2025, 6, 10, 13, 50, 60));
+        taskManager.createTask(new Task("Task3", "Description3", 2025, 7, 13, 1, 35, 70));
+        taskManager.createTask(new Task("Task4", "Description4", 2025, 8, 13, 1, 30, 140));
 
-        int epic1 = taskManager.createEpic(new Epic("Путевка.", Status.NEW, "Выбрать маршрут."));
-        int subtask11 = taskManager.createSubtask(new Subtask("Билеты.", Status.NEW, "Купить.", 3));
-        int subtask12 = taskManager.createSubtask(new Subtask("Личные вещи.", Status.NEW, "Собрать чемодан.", 3));
+        taskManager.createEpic(new Epic("Epic5", "Description1"));
+        taskManager.createEpic(new Epic("Epic6", "Description2"));
+        taskManager.createEpic(new Epic("Epic7", "Description3"));
 
-        int epic2 = taskManager.createEpic(new Epic("Изучить новую специальность.", Status.NEW, "Освоить язык программирования Java."));
-        int subtask21 = taskManager.createSubtask(new Subtask("Учеба.", Status.NEW, "Пройти курс на Яндекс-Практикум.", 6));
+        taskManager.createSubtask(new Subtask("Subtask8", "Description1", 2025, 6, 1, 12, 14, 30, 5));
+        taskManager.createSubtask(new Subtask("Subtask9", "Description2", 2025, 7, 1, 10, 14, 30, 5));
+        taskManager.createSubtask(new Subtask("Subtask10", "Description3", 2025, 8, 1, 12, 14, 30, 6));
+        taskManager.createSubtask(new Subtask("Subtask11", "Description4", 2025, 9, 22, 12, 19, 30, 7));
+        taskManager.createSubtask(new Subtask("Subtask12", "Description5", 2025, 9, 22, 14, 14, 30, 7));
 
-        System.out.println(taskManager.getTasks());
-        System.out.println(taskManager.getEpics());
-        System.out.println(taskManager.getSubtasks());
-        System.out.println("================");
+        taskManager.createTask(new Task("Task", "Task"));
+        taskManager.updateTask(13, new Task("Task13", "Description4", 2025, 10, 13, 1, 30, 140));
 
-        taskManager.deleteTasks();
+        System.out.println(taskManager.getPrioritizedTasks());
 
-        TaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(file);
-        System.out.println(fileBackedTaskManager.getTasks());
-        System.out.println(fileBackedTaskManager.getEpics());
-        System.out.println(fileBackedTaskManager.getSubtasks());
     }
 }
