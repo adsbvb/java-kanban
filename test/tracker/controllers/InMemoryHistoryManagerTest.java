0c72d5e9d558541d7b6bffe2e3897cb54a669ed1
Daggett <adsbvb@yandex.ru>
2025-04-17 18:00:37 +0500

    Добавить для HistoryManager — тесты для всех методов интерфейса. Граничные условия: a. Пустая история задач. b. Дублирование. c. Удаление из истории: начало, середина, конец.

--- test/tracker/controllers/InMemoryHistoryManagerTest.java
+++ test/tracker/controllers/InMemoryHistoryManagerTest.java
@@ -1,63 +1,72 @@
 package tracker.controllers;
 
 import org.junit.jupiter.api.*;
-import tracker.model.Epic;
 import tracker.model.Status;
-import tracker.model.Subtask;
 import tracker.model.Task;
 
-import java.io.File;
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
 
-    private File file;
-    private TaskManager taskManager;
-    private Task testTask;
-    private Epic testEpic;
-    private Subtask testSubtask;
-    private int idTask;
-    private int idEpic;
-    private int idSubtask;
+    private HistoryManager history;
 
     @BeforeEach
-    void setUp() throws IOException{
-        file = File.createTempFile("test_", ".csv");
-        taskManager = Managers.getDefault(file);
-        Assertions.assertNotNull(taskManager);
-
-        testTask = new Task(1, "TaskName", Status.NEW, "TaskDescription");
-        idTask = taskManager.createTask(testTask);
-
-        testEpic = new Epic(2, "EpicName", Status.NEW, "EpicDescription");
-        idEpic = taskManager.createEpic(testEpic);
-
-        testSubtask = new Subtask(3, "SubtaskName", Status.NEW, "SubtaskDescription", idEpic);
-        idSubtask = taskManager.createSubtask(testSubtask);
+    void setUp() {
+        history = new InMemoryHistoryManager();
     }
 
     @Test
-    void testAddHistory() {
-        taskManager.getTask(idTask);
-        taskManager.getEpic(idEpic);
-        taskManager.getSubtask(idSubtask);
-        Assertions.assertEquals(taskManager.getHistory().getFirst(), taskManager.getTask(idTask), "Предыдущая задача не сохранилась!");
+    void testAddToHistory() {
+        Task task1 = new Task(1, "name1" , "description1", Status.NEW);
+        Task task2 = new Task(2, "name2" , "description2", Status.NEW);
+        history.add(task1);
+        history.add(task2);
+        assertEquals(task1, history.getHistory().getFirst(), "Задача №1 не была записана в историю задач!");
+        assertEquals(task2, history.getHistory().getLast(), "Задача №2 не была записана в историю задач!");
     }
 
     @Test
-    void testRemove() {
-        taskManager.getTask(idTask);
-        taskManager.remove(idTask);
-        Assertions.assertEquals(0, taskManager.getHistory().size(), "Задача не была удалена из списка!");
+    void checkReentryTaskInHistory() {
+        Task task = new Task("Name", "Description");
+        history.add(task);
+        history.add(task);
+        assertEquals(1, history.getHistory().size(), "Задача была записана дважды в историю задач!");
     }
 
     @Test
-    void checkReEntryTaskInHistoryList() {
-        taskManager.getTask(idTask);
-        taskManager.getEpic(idEpic);
-        taskManager.getSubtask(idSubtask);
-        taskManager.getTask(idTask);
-        Assertions.assertNotEquals(taskManager.getHistory().getFirst(), taskManager.getTask(idTask), "Задача была записана дважды!");
-        Assertions.assertEquals(taskManager.getHistory().getLast(), taskManager.getTask(idTask), "Задача не была перезаписана в список!");
+    void testReturnEmptyListHistory() {
+        assertTrue(history.getHistory().isEmpty(), "История задач не пуста!");
+    }
+
+    @Test
+    void testRemoveFromHistoryStart() {
+        Task task1 = new Task(1, "Name", "Description", Status.NEW);
+        Task task2 = new Task(2, "Name", "Description", Status.NEW);
+        history.add(task1);
+        history.add(task2);
+        history.remove(task1.getId());
+        assertFalse(history.getHistory().contains(task1), "Задача не была удалена с начала списка истории задач!");
+    }
+
+    @Test
+    void testRemoveFromHistoryMiddle() {
+        Task task1 = new Task(1, "Name", "Description", Status.NEW);
+        Task task2 = new Task(2, "Name", "Description", Status.NEW);
+        Task task3 = new Task(3, "Name", "Description", Status.NEW);
+        history.add(task1);
+        history.add(task2);
+        history.add(task3);
+        history.remove(task2.getId());
+        assertFalse(history.getHistory().contains(task2), "Задача не была удалена из середины списка истории задач!");
+    }
+
+    @Test
+    void testRemoveFromHistoryEnd() {
+        Task task1 = new Task(1, "Name", "Description", Status.NEW);
+        Task task2 = new Task(2, "Name", "Description", Status.NEW);
+        history.add(task1);
+        history.add(task2);
+        history.remove(task2.getId());
+        assertFalse(history.getHistory().contains(task2), "Задача не была удалена в конце списка истории задач!");
     }
 }
